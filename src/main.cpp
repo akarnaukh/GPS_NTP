9889aa988e3296afd6c06bfb8c9a5025314cbeb9
akarnaukh <a.karnauh@gmail.com>
2024-07-05 22:53:44 +0300

    master

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,23 @@ void setup() {
   GPSSerial.begin(9600);
   delay(200);
   GPSSerial.flush();
+  // LM: These sentences are the same for the NEO-M8N   -  Comments added
+  GPSSerial.write("$PUBX,40,GLL,0,0,0,0,0,0*5C\r\n");   // Lat/Lon data
+  GPSSerial.write("$PUBX,40,VTG,0,0,0,0,0,0*5E\r\n");   // Vector track and speed over ground
+  GPSSerial.write("$PUBX,40,GSV,0,0,0,0,0,0*59\r\n");   // Detailed satellite data
+  GPSSerial.write("$PUBX,40,GGA,0,0,0,0,0,0*5A\r\n");   // Fix information 
+  GPSSerial.write("$PUBX,40,GSA,0,0,0,0,0,0*4E\r\n");   // Overall satellite data
 }
 
 void loop() {
-  long now = (long)millis();
+  //long now = (long)millis();
   wdt_reset();
   /*if ((now - interval) > 1000) {
     Serial.print("Tttt...\n");
     interval = (long)millis();
   }*/
   while (GPSSerial.available() > 0)
-    if (gps.encode(GPSSerial.read()))
-      displayInfo();
+    if (gps.encode(GPSSerial.read())) displayInfo();
 
   if (millis() > 5000 && gps.charsProcessed() < 10) {
     Serial.println(F("No GPS detected: check wiring."));
@@ -28,17 +33,19 @@ void loop() {
 }
 
 void displayInfo() {
- /* Serial.print(F("Location: ")); 
+  //Serial.print(F("Sats=")); Serial.print(gps.satellites.value());
+  //Serial.print(F(" Alt=")); Serial.print(gps.altitude.isValid());
+  Serial.print(F("Location: ")); 
   if (gps.location.isValid()) {
     Serial.print(gps.location.lat(), 6);
     Serial.print(F(","));
     Serial.print(gps.location.lng(), 6);
   } else {
     Serial.print(F("INVALID"));
-  } */ 
+  }
 
   Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid()) {
+  if (gps.date.isValid() && (gps.date.year() > 2000)) {
     Serial.print(gps.date.month());
     Serial.print(F("/"));
     Serial.print(gps.date.day());
